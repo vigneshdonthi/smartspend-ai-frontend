7b6d65a81c667312869bda2f23eb4a3eba33644f
Vignesh donthi <Vigneshdonthi1@gmail.com>
2026-06-30 23:33:32 +0530

    Setup React frontend with Tailwind and shadcn UI

---
+++ src/pages/AIInsights.jsx
@@ -0,0 +1,5 @@
+function AIInsights() {
+  return <h1>AI Insights</h1>;
+}
+
+export default AIInsights;
---
+++ src/pages/Expenses.jsx
@@ -0,0 +1,5 @@
+function Expenses() {
+  return <h1>Expenses</h1>;
+}
+
+export default Expenses;
